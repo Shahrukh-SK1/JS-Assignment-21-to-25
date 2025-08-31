36f723c0b100913f9d7c2758c87de5c824bd1bf4
Shahrukh-SK1 <sharukhayub6111@gmail.com>
2025-09-01 00:06:52 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,75 @@
+// Answer No. 1
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+
+// var fullName = [firstName + " " + lastName];
+
+// alert("Hi, " + fullName + " Welcome");
+
+
+// Answer No. 2
+
+// var phoneModel = prompt("Enter your favourite phone full name with model");
+
+// var modelLenght = phoneModel.length;
+
+
+// document.write("My favourite phone is: " + phoneModel + "<br>");
+// document.write("Length of String: " + modelLenght + "<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+
+
+// Answer No. 3
+    
+var word = "Pakistani";
+
+document.write("<br>");
+        
+document.write("String: " + word + "<br>");
+document.write("Index of 'n '" + word.indexOf("n") + "<br>");
+
+document.write("<br>");
+
+// Answer No. 4
+
+var string = "Hello World"
+
+document.write("String " + string + "<br>");
+document.write("Last index of 'l': " + string.lastIndexOf("l"));
+
+document.write("<br>");
+
+
+// Answer No. 5
+
+document.write("<br><br>");
+
+var pak = "Pakistan"
+
+document.write("String " + pak + "<br>");
+document.write("Chart at index 3: " + pak.charAt(3));
+
+document.write("<br><br>");
+
+// Answer No. 7
+
+var city = "Hyderabad"
+
+document.write("City: " + city + "<br>");
+ 
+var cityRep = city.replace("Hyder", "Islam");
+document.write("After replacement: " + cityRep);
+document.write("<br><br>");
+
+// Answer No. 8
+
+var message = "Ali and Sami are best friends. They play cricket and football together.";
+document.write("Original message: " + message + "<br>");
+document.write("<br><br>");
+var messReplaced = message.replace(/and/g, "&");
+document.write("Replaced message: " + messReplaced + "<br>");
+
+document.write("<br><br>");
